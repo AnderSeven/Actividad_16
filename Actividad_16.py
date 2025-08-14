f28cac8e1ca7b8a7dd4885b236f8f5284cc8b595
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-08-13 19:45:06 -0600

    se creo el menu y la clase para registrar a los estudiantes

--- Actividad_16.py
+++ Actividad_16.py
@@ -16,3 +16,52 @@ class usuarios():
 
     def info_usuarios(self):
         return f"Nombre: {self.nombre}, Carnet: {self.carnet}, Carrera: {self.carrera}"
+
+class registro_estudiantes:
+    def __init__(self):
+        self.lista_biblioteca = []
+
+    def registrar(self):
+        nombre = input("Ingrese el nombre: ")
+        carnet = int(input("Ingrese el carnet: "))
+        carrera = input("Ingrese la carrera: ")
+
+    def info(self):
+        if len(self.lista_biblioteca) > 0:
+            print("---Lista de Estudiantes---")
+            for i in self.lista_biblioteca:
+                print(f"")
+        else:
+            print("No hay estudiantes registrados")
+
+class prestamos():
+    pass
+
+class devoluciones():
+    pass
+
+
+a = False
+opciones = 0
+while a == False:
+    print("---Menu---")
+    print("1. Agregar estudiante")
+    print("2. Agregar libro")
+    print("3. Prestar libro")
+    print("4. Devolver libro")
+    print("5. Salir")
+    opciones = int(input("Elija una opcion: "))
+    match opciones:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            print("Gracias por usar el sistema")
+            a = True
+        case _:
+            print("Opcion invalida")
